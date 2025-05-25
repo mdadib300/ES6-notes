f77a09989a6dfa59eb4c4d366c3b62bf5fd5b872
unknown <mdadib300@gmail.com>
2025-05-25 15:14:04 +0600

    variables added

---
+++ variable.js
@@ -0,0 +1,14 @@
+// Variables - var, let, const
+// We'll use var very hardly
+// let - value of the variable can be changed
+// const - value of the variable is constant
+
+let age = 12;
+age = 13;
+console.log(age)
+
+const name = 'Adib';
+console.log(name)
+name = 'Abid';
+// console.log(name)
+// Error!
